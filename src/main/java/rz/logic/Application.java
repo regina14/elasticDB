564f1c5cd52a47c552805021827f28d3d4aa4762
Regina <reginazhang14@gmail.com>
2018-01-09 22:04:45 -0700

    Update Application.java

--- src/main/java/rz/logic/Application.java
+++ src/main/java/rz/logic/Application.java
@@ -2,13 +2,6 @@ package rz.logic;
 
 import java.util.ArrayList;
 
-/**
- * Created by IntelliJ IDEA.
- * User: pxiong
- * Date: Jun 28, 2010
- * Time: 12:07:35 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Application {
     public String path;
     public String writeQueue;
